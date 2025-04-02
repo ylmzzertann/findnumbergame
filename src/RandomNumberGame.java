ad4d13d9960ca5234beccf1093cc413c96165ac4
ylmzzertann <ertanylmz2012@hotmail.com>
2025-04-01 23:48:54 -0400

    initial commit

--- src/RandomNumberGame.java
+++ src/RandomNumberGame.java
@@ -1,7 +1,6 @@
 import java.util.Random;
 import java.util.Scanner;
 import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.atomic.AtomicInteger;
 
 class NumberGenerator {
     private Random random = new Random();
@@ -79,64 +78,81 @@ public class RandomNumberGame {
         String lastName = scanner.nextLine();
         System.out.println("Merhaba, " + firstName + " " + lastName + "! Oyun başlıyor...\n");
 
-        System.out.println("Zorluk seviyesini seçin: 1 - Kolay (90 sn), 2 - Orta (60 sn), 3 - Zor (30 sn)");
-        int level = scanner.nextInt();
-        int countdown = switch (level) {
-            case 1 -> 90;
-            case 2 -> 60;
-            case 3 -> 30;
-            default -> 60;
-        };
-
-        NumberGenerator generator = new NumberGenerator();
-        generator.generateNumbers();
-        int[] numbers = generator.getNumbers();
-        int targetNumber = generator.getTargetNumber();
-
-        System.out.print("Oluşturulan sayı dizisi: ");
-        for (int num : numbers) {
-            System.out.print(num + " ");
-        }
-        System.out.println("\nHedeflenen 3 basamaklı sayı: " + targetNumber);
-
-        AtomicBoolean stopRequested = new AtomicBoolean(false);
-        Timer timer = new Timer();
-        UserInputHandler inputHandler = new UserInputHandler();
-
-        System.out.println("Lütfen süre dolmadan hedef sayıyı tahmin edin! Süreyi durdurmak için '0' yazın.");
-        Thread timerThread = new Thread(() -> timer.startCountdown(countdown, stopRequested));
-        timerThread.start();
-
-        long startTime = System.currentTimeMillis();
-        int userGuess = inputHandler.getUserGuess(scanner, stopRequested);
-        long endTime = System.currentTimeMillis();
+        boolean playAgain;
+        int totalScore = 0;
         
-        stopRequested.set(true);
-        try {
-            timerThread.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        
-        int elapsedSeconds = (int) ((endTime - startTime) / 1000);
-        int score = 0;
-        int diff = Math.abs(userGuess - targetNumber);
-        
-        if (userGuess == targetNumber) {
-            score = (elapsedSeconds <= 15) ? 10 : (elapsedSeconds <= 30) ? 7 : 5;
-        } else {
-            score = switch (diff) {
-                case 1 -> 3;
-                case 2 -> 2;
-                case 3 -> 1;
-                default -> 0;
+        do {
+            System.out.println("Zorluk seviyesini seçin: 1 - Kolay (90 sn), 2 - Orta (60 sn), 3 - Zor (30 sn)");
+            int level = scanner.nextInt();
+            int countdown = switch (level) {
+                case 1 -> 90;
+                case 2 -> 60;
+                case 3 -> 30;
+                default -> 60;
             };
-        }
+
+            NumberGenerator generator = new NumberGenerator();
+            generator.generateNumbers();
+            int[] numbers = generator.getNumbers();
+            int targetNumber = generator.getTargetNumber();
+
+            System.out.print("Oluşturulan sayı dizisi: ");
+            for (int num : numbers) {
+                System.out.print(num + " ");
+            }
+            System.out.println("\nHedeflenen 3 basamaklı sayı: " + targetNumber);
+
+            AtomicBoolean stopRequested = new AtomicBoolean(false);
+            Timer timer = new Timer();
+            UserInputHandler inputHandler = new UserInputHandler();
+
+            System.out.println("Lütfen süre dolmadan hedef sayıyı tahmin edin! Süreyi durdurmak için '0' yazın.");
+            Thread timerThread = new Thread(() -> timer.startCountdown(countdown, stopRequested));
+            timerThread.start();
+
+            long startTime = System.currentTimeMillis();
+            int userGuess = inputHandler.getUserGuess(scanner, stopRequested);
+            long endTime = System.currentTimeMillis();
+            
+            stopRequested.set(true);
+            try {
+                timerThread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            
+            int elapsedSeconds = (int) ((endTime - startTime) / 1000);
+            int score = 0;
+            int diff = Math.abs(userGuess - targetNumber);
+            
+            if (userGuess == targetNumber) {
+                score = (elapsedSeconds <= 15) ? 10 : (elapsedSeconds <= 30) ? 7 : 5;
+            } else {
+                score = switch (diff) {
+                    case 1 -> 3;
+                    case 2 -> 2;
+                    case 3 -> 1;
+                    default -> 0;
+                };
+            }
+            totalScore += score;
+            
+            System.out.println("Tahmininiz: " + userGuess + " | Hedef: " + targetNumber);
+            System.out.println("Puanınız: " + score);
+            System.out.println("Toplam Puanınız: " + totalScore);
+            System.out.println("Tahmininiz hedef sayıya " + diff + " kadar uzaklıkta.");
+            
+            if (diff <= 3) {
+                System.out.print("Oyunu tekrar oynamak ister misiniz? (Evet için 'E' / Hayır için 'H'): ");
+                char response = scanner.next().charAt(0);
+                playAgain = (response == 'E' || response == 'e');
+            } else {
+                playAgain = false;
+            }
+        } while (playAgain);
         
-        System.out.println("Tahmininiz: " + userGuess + " | Hedef: " + targetNumber);
-        System.out.println("Puanınız: " + score);
-        System.out.println("Tahmininiz hedef sayıya " + diff + " kadar uzaklıkta.");
-        System.out.println("Oyun sona erdi. Program kapanıyor...");
+        System.out.println("Oyun sona erdi. Toplam puanınız: " + totalScore);
+        System.out.println("Program kapanıyor...");
         System.exit(0);
     }
 }
